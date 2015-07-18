da91009f2d7a0f2600c349cde306ade97fe5ffd1
Adrian-Tudor Panescu <adrian@panescu.com>
2015-07-18 12:15:47 +0300

    Heroku/ local time offset

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ var path = require('path');
 var quotes = require('./quotes.json');
 
 
+var currentTime = function currentTime() {
+	if (process.env.PORT) {
+		// running on Heroku, we need to offset the time
+		return moment().utcOffset(+180);
+	}
+	return moment();
+};
+
 var quote = function quote() {
 	var result = 'Sorry, I forgot to add the quote for today; you win!';
 	for (var k in quotes) {
 		if (quotes.hasOwnProperty(k)) {
-			if (moment().startOf('day').isSame(moment(k, 'DD.MM.YYYY'))) {
+			if (currentTime().startOf('day').isSame(moment(k, 'DD.MM.YYYY'))) {
 				result = quotes[k];
 				break;
 			}
@@ -22,7 +30,7 @@ var quote = function quote() {
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(path.join(__dirname, '/static')));
 
@@ -32,7 +40,7 @@ app.set('view engine', 'ejs');
 app.get('/', function indexReq(request, response) {
 	response.render('index', {
 		'quote': quote(),
-		'time': moment().utcOffset(+180)
+		'time': currentTime()
 	});
 });
 
