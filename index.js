6fbc6ad6324fc43a04848ae12cc83607c096bd37
Adrian-Tudor Panescu <adrian@panescu.com>
2015-07-18 11:20:21 +0300

    inital application dump

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+
+var quote = function quote() {
+	return 'Lorem ipsum dolor';
+};
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, '/static')));
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function indexReq(request, response) {
+  response.render('index', {'quote': quote()});
+});
+
+app.listen(app.get('port'), function start() {
+  console.log('Node app is running on port', app.get('port'));
+});
